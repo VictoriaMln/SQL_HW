931c55220a208b7a638b73205d123d9b8b972516
VictoriaMln <vitoriqa@gmail.com>
2025-03-24 19:30:13 +0300

    Update vehicles.sql

--- vehicles.sql
+++ vehicles.sql
@@ -80,9 +80,98 @@ INSERT INTO Bicycle (serial_number, model, gear_count, price, type) VALUES
 
 /*Задача 1
 Условие:
-  Найдите производителей (maker) и модели всех мотоциклов, которые имеют мощность более 150 лошадиных сил, 
-  стоят менее 20 тысяч долларов и являются спортивными (тип Sport). 
-  Также отсортируйте результаты по мощности в порядке убывания. *\
+  Найдите производителей (maker) и модели всех мотоциклов, которые имеют мощность более 150 лошадиных сил, стоят менее 
+  20 тысяч долларов и являются спортивными (тип Sport). Также отсортируйте результаты по мощности в порядке убывания. *\
 
 -- Решение:
-  
+SELECT 
+	vehicle.maker,
+  motorcycle.model
+FROM 
+	Motorcycle
+JOIN 
+	vehicle on motorcycle.model = vehicle.model
+WHERE
+	motorcycle.horsepower > 150 
+  && motorcycle.price < 20000 
+  && motorcycle.type = 'Sport'
+ORDER BY 
+	motorcycle.horsepower DESC;
+
+/*Задача 2
+Условие:
+  Найти информацию о производителях и моделях различных типов транспортных средств (автомобили, мотоциклы и велосипеды), 
+  которые соответствуют заданным критериям.
+
+  Автомобили:
+  Извлечь данные о всех автомобилях, которые имеют:
+    Мощность двигателя более 150 лошадиных сил. Объем двигателя менее 3 литров. Цену менее 35 тысяч долларов.
+    В выводе должны быть указаны производитель (maker), номер модели (model), мощность (horsepower), 
+    объем двигателя (engine_capacity) и тип транспортного средства, который будет обозначен как Car.
+
+  Мотоциклы:
+  Извлечь данные о всех мотоциклах, которые имеют:
+    Мощность двигателя более 150 лошадиных сил. Объем двигателя менее 1,5 литров. Цену менее 20 тысяч долларов.
+    В выводе должны быть указаны производитель (maker), номер модели (model), мощность (horsepower), объем двигателя (engine_capacity)
+    и тип транспортного средства, который будет обозначен как Motorcycle.
+
+  Велосипеды:
+  Извлечь данные обо всех велосипедах, которые имеют:
+    Количество передач больше 18. Цену менее 4 тысяч долларов.
+    В выводе должны быть указаны производитель (maker), номер модели (model), а также NULL для мощности и объема двигателя,
+    так как эти характеристики не применимы для велосипедов. Тип транспортного средства будет обозначен как Bicycle.
+
+  Сортировка:
+    Результаты должны быть объединены в один набор данных и отсортированы по мощности в порядке убывания. Для велосипедов, 
+    у которых нет значения мощности, они будут располагаться внизу списка. *\
+
+-- Решение:
+(SELECT 
+	vehicle.maker,
+  car.model,
+  car.horsepower,
+  car.engine_capacity,
+  vehicle.type
+FROM 
+	car
+JOIN 
+	vehicle on car.model = vehicle.model
+WHERE
+	car.horsepower > 150 
+  && car.price < 35000 
+  && car.engine_capacity < 3)
+    
+UNION ALL
+
+(SELECT 
+	vehicle.maker,
+  motorcycle.model,
+  motorcycle.horsepower,
+  motorcycle.engine_capacity,
+  vehicle.type
+FROM 
+	motorcycle
+JOIN 
+	vehicle on motorcycle.model = vehicle.model
+WHERE
+	motorcycle.horsepower > 150 
+  && motorcycle.price < 20000 
+  && motorcycle.engine_capacity < 1.5)
+    
+UNION ALL
+
+(SELECT 
+	vehicle.maker,
+  bicycle.model,
+  NULL AS horsepower,
+  NULL AS engine_capacity,
+  vehicle.type
+FROM 
+	bicycle
+JOIN 
+	vehicle on bicycle.model = vehicle.model
+WHERE
+	bicycle.gear_count > 18 
+  && bicycle.price < 4000)
+    
+ORDER BY horsepower DESC;
